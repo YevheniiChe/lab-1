803df592d82baae545e7a6b24084f2602b08fb1d
YevheniiChe <127835870+YevheniiChe@users.noreply.github.com>
2023-06-11 15:57:19 +0300

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "amazon_linux" {
  
  filter {
  name = "name"
- values = ["amzn2-ami-kernel-5.10-hvm-x86_64-gp2"]
+ values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
  }
  owners = ["amazon"]
 }
